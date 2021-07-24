babfe4154d66503d4419480892ac07e6f36b199b
momosetti <zinasetti@gmail.com>
2021-07-24 18:50:24 +0100

    feat: style headline and taggline

--- src/components/layout/sectionWrapper.tsx
+++ src/components/layout/sectionWrapper.tsx
@@ -5,6 +5,14 @@ const Section = styled.section`
   padding: 1em;
   margin: 1em;
   border: 1px solid var(--white-grey);
+  h1 {
+    font-size: 2em;
+  }
+  p {
+    color: var(--dark-grey);
+    font-size: 1.1em;
+    font-weight: 600;
+  }
 `;
 
 const SectionWrapper: React.FunctionComponent = ({ children }) => {
