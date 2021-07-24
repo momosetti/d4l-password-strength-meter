51bfbb934ab5cea32b9e97773000d60a1334bd21
momosetti <zinasetti@gmail.com>
2021-07-24 18:51:48 +0100

    refactor: add section shadow style

--- src/components/layout/sectionWrapper.tsx
+++ src/components/layout/sectionWrapper.tsx
@@ -5,6 +5,7 @@ const Section = styled.section`
   padding: 1em;
   margin: 1em;
   border: 1px solid var(--white-grey);
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
   h1 {
     font-size: 2em;
   }
