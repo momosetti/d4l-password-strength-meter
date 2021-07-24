84d6e0f139183376f9f7f0d0477d954afe39f53a
momosetti <zinasetti@gmail.com>
2021-07-24 17:56:17 +0100

    feat: implement global style and remixicon globaly

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "remixicon/fonts/remixicon.css";
+import GlobalStyle from "./globalStyle";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
