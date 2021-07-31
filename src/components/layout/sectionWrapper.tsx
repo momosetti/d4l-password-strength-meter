ffc67eadd69b72ee8c95b297fa271ef17f02d948
momosetti <zinasetti@gmail.com>
2021-07-31 02:14:52 +0100

    refactor: edit the main section header and tagline style selectors

--- src/components/layout/sectionWrapper.tsx
+++ src/components/layout/sectionWrapper.tsx
@@ -6,10 +6,10 @@ const Section = styled.section`
   margin: 1em;
   border: 1px solid var(--white-grey);
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-  h1 {
+  & > h1 {
     font-size: 2em;
   }
-  p {
+  & > p {
     color: var(--dark-grey);
     font-size: 1.1em;
     font-weight: 600;
