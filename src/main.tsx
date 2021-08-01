6ee0817e3b0b26cb82a40da80a64b80be110dcf8
momosetti <zinasetti@gmail.com>
2021-08-01 20:18:56 +0100

    refactor: implement react axe

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import "remixicon/fonts/remixicon.css";
 import GlobalStyle from "./globalStyle";
 import App from "./App";
 
+import axe from "@axe-core/react";
+if (process.env.NODE_ENV !== "production") {
+  axe(React, ReactDOM, 1000);
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
