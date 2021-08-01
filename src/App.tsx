0373dc510efa4915a61507c19def04a503e1dc9e
momosetti <zinasetti@gmail.com>
2021-08-01 20:26:05 +0100

    refactor: wrap the main App with CenterHorizontally styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import EmailInput from "./components/emailInput";
 import SectionWrapper from "./components/layout/sectionWrapper";
 import PasswordInput from "./components/passwordInput";
@@ -6,15 +7,22 @@ import RegisterFormWrapper from "./components/layout/registerFormWrapper";
 
 const App: React.FunctionComponent = () => {
   return (
-    <SectionWrapper>
-      <h1>Register</h1>
-      <p>Create your personal account</p>
-      <RegisterFormWrapper>
-        <EmailInput />
-        <PasswordInput />
-      </RegisterFormWrapper>
-    </SectionWrapper>
+    <CenterHorizontally>
+      <SectionWrapper>
+        <h1>Register</h1>
+        <p>Create your personal account</p>
+        <RegisterFormWrapper>
+          <EmailInput />
+          <PasswordInput />
+        </RegisterFormWrapper>
+      </SectionWrapper>
+    </CenterHorizontally>
   );
 };
 
 export default App;
+
+const CenterHorizontally = styled.div`
+  display: flex;
+  justify-content: center;
+`;
