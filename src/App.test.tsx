f37bb714572d494b5ce9e35d779c6b86988afcec
momosetti <zinasetti@gmail.com>
2021-08-07 17:52:59 +0100

    test: add test suites to the main App component

---
+++ src/App.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { axe, toHaveNoViolations } from "jest-axe";
+
+describe("test the main App", () => {
+  test("renders App component correctly", () => {
+    render(<App />);
+    expect(screen.getByText("Register")).toBeInTheDocument();
+  });
+
+  test("check if there's any accessiblity violations", async () => {
+    expect.extend(toHaveNoViolations);
+
+    const { container } = render(<App />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+});
