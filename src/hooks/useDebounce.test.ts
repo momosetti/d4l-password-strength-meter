aa1160809d4fb1662b6887750b3de0bfcb2c2f0f
momosetti <zinasetti@gmail.com>
2021-08-18 05:11:04 +0100

    test: maintain useDebounce test suites
    - use `jest.useFakeTimers` instead of async/await instruction

--- src/hooks/useDebounce.test.ts
+++ src/hooks/useDebounce.test.ts
@@ -1,8 +1,15 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import useDebounce from "./useDebounce";
 
-describe("should return value when recall useDabounce after 1/2 second", () => {
-  test("assert init value will be assinged", () => {
+beforeAll(() => {
+  jest.useFakeTimers();
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
+describe("should return value when recall useDebounce after 1/2 second", () => {
+  test("assert init value will be assigned", () => {
     const { result } = renderHook((value) => useDebounce(value, 2000), {
       initialProps: "init",
     });
@@ -18,12 +25,14 @@ describe("should return value when recall useDabounce after 1/2 second", () => {
 
     // call the hook with new props argument (recalling useDebounce)
     rerender("abc");
-
     // the debounced value isn't updated yet
     expect(result.current).toEqual("init");
 
     // wait 1 second until rerender("abc") return "abc"
-    await new Promise((r) => setTimeout(r, 1000));
+    // await new Promise((r) => setTimeout(r, 1000));
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
 
     // check the new returned value
     expect(result.current).toEqual("abc");
